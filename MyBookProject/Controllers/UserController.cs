1c3694a8c225efe7a4a75160ab3e56b2ea18cf87
kunal swami <kunalswami29@gmail.com>
2021-04-06 22:35:09 +0530

    Add project files.

---
+++ MyBookProject/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyBookProject.Models;
+
+namespace MyBookProject.Controllers
+{
+    public class UserController : Controller
+    {
+        private OnlineBookStoreDbEntities context;
+
+        public UserController()
+        {
+            context = new OnlineBookStoreDbEntities();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            context.Dispose();
+        }
+
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return HttpNotFound();
+            }
+
+            context.Users.Add(user);
+            context.SaveChanges();
+
+            ViewBag.Message = "Thanks For Registering,This Is Your UserId";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ MyBookProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyBookProject.MvcApplication" Language="C#" %>
