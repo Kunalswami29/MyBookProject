3a52811dbd5bda4fad81fb349fee8ab6e7a46c0b
kunal swami <kunalswami29@gmail.com>
2021-04-15 09:25:24 +0530

    Home Index page Updated Resource List

--- MyBookProject/Controllers/HomeController.cs
+++ MyBookProject/Controllers/HomeController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyBookProject.Models;
 
 namespace MyBookProject.Controllers
 {
     public class HomeController : Controller
     {
-       
+        private OnlineBookStoreDbEntities context;
+
+        public HomeController()
+        {
+            context = new OnlineBookStoreDbEntities();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            context.Dispose();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var books = context.Books.ToList();
+
+            return View(books);
         }
 
         public ActionResult About()
